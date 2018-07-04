86682b5ad4fcda04c6e14548d4e0736d43653d27
nathanbreitsch <nathanbreitsch@gmail.com>
2018-07-04 01:36:53 -0400

    fixup! Make dataset subclass and visualization for keypoint data

--- keypoint_detection/dataset.py
+++ keypoint_detection/dataset.py
@@ -28,10 +28,11 @@ class KeypointsDataset(Dataset):
         item = self.manifest[i]
         image_path = os.path.join(self.root_dir, item.image_name)
         image_ndarray = imageio.imread(image_path)
+        image_height, image_width, _ = image_ndarray.shape
 
         return Sample(
             image = image_ndarray_to_tensor(image_ndarray),
-            keypoints = keypoints_ndarray_to_tensor(item.keypoints)
+            keypoints = keypoints_ndarray_to_tensor(item.keypoints, image_height, image_width)
         )
 
 def image_ndarray_to_tensor(image):
@@ -39,25 +40,29 @@ def image_ndarray_to_tensor(image):
     raw_tensor = torch.from_numpy(float_ndarray)
     normalized = raw_tensor / 255
     no_alpha = normalized[:, :, :3]
-    canonical_torch_axes = no_alpha.permute(2, 0, 1)
+    canonical_torch_axes = no_alpha.permute(2, 0, 1).unsqueeze(0)
     return canonical_torch_axes
 
 def image_tensor_to_ndarray(tensor):
-    canonical_numpy_axes = tensor.permute(1, 2, 0)
+    canonical_numpy_axes = tensor.squeeze(0).permute(1, 2, 0)
     denormalized = 255 * canonical_numpy_axes
     numpy_array = denormalized.numpy()
     int_numpy_array = numpy_array.astype(np.uint8)
     return int_numpy_array
 
-def keypoints_ndarray_to_tensor(keypoints_ndarray):
+def keypoints_ndarray_to_tensor(keypoints_ndarray, image_height, image_width):
     float_ndarray = keypoints_ndarray.astype(np.float32)
-    keypoints_tensor = torch.from_numpy(float_ndarray)
-    normalized = (keypoints_tensor - 100) / 50
-    return normalized
+    keypoints_tensor = torch.from_numpy(float_ndarray.copy())
+    keypoints_tensor[1::2] /= (2 * image_height)
+    keypoints_tensor[0::2] /= (2 * image_width)
+    keypoints_tensor -=1
+    return keypoints_tensor
 
-def keypoints_tensor_to_ndarray(keypoints):
-    denormalized = keypoints * 50 + 100
-    keypoints_ndarray = denormalized.numpy()
+def keypoints_tensor_to_ndarray(keypoints, image_height, image_width):
+    keypoints_ndarray = keypoints.numpy().copy()
+    keypoints_ndarray += 1
+    keypoints_ndarray[1::2] *= image_height * 2
+    keypoints_ndarray[0::2] *= image_width * 2
     int_ndarray = keypoints_ndarray.astype(np.int32)
     return int_ndarray
 
--- keypoint_detection/visualization.py
+++ keypoint_detection/visualization.py
@@ -4,9 +4,12 @@ from .dataset import image_tensor_to_ndarray, keypoints_tensor_to_ndarray
 def plot_image(image_tensor, keypoints_tensor=None):
     fig, ax = plt.subplots(figsize=(12, 10))
     image_ndarray = image_tensor_to_ndarray(image_tensor)
+    image_height, image_width, _ = image_ndarray.shape
     ax.imshow(image_ndarray)
     if keypoints_tensor is not None:
-        keypoints_ndarray = keypoints_tensor_to_ndarray(keypoints_tensor)
+        print(keypoints_tensor.max())
+        keypoints_ndarray = keypoints_tensor_to_ndarray(keypoints_tensor, image_height, image_width)
+        print(keypoints_ndarray.max())
         keypoints_matrix = keypoints_ndarray.reshape((-1, 2))
         ax.scatter(keypoints_matrix[:, 0], keypoints_matrix[:, 1])
 
