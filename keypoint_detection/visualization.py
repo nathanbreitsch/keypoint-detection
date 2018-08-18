4ed636e6ec684ab7b60ea6df25ddb3418a385c80
nathanbreitsch <nathanbreitsch@gmail.com>
2018-08-18 19:18:36 -0400

    Allow ax and fig to be passed into plot_image

--- keypoint_detection/visualization.py
+++ keypoint_detection/visualization.py
@@ -1,8 +1,9 @@
 from matplotlib import pyplot as plt
 from .dataset import image_tensor_to_ndarray, keypoints_tensor_to_ndarray
 
-def plot_image(image_tensor, keypoints_tensor=None):
-    fig, ax = plt.subplots(figsize=(12, 10))
+def plot_image(image_tensor, keypoints_tensor=None, pred_keypoints_tensor=None, ax=None, fig=None):
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(12, 10))
     image_ndarray = image_tensor_to_ndarray(image_tensor)
     image_height, image_width, n_channels = image_ndarray.shape
 
@@ -16,3 +17,9 @@ def plot_image(image_tensor, keypoints_tensor=None):
         keypoints_matrix = keypoints_ndarray.reshape((-1, 2))
         ax.scatter(keypoints_matrix[:, 0], keypoints_matrix[:, 1], marker='.', c='g')
 
+    if pred_keypoints_tensor is not None:
+        pred_keypoints_ndarray = keypoints_tensor_to_ndarray(pred_keypoints_tensor, image_height, image_width)
+        pred_keypoints_matrix = pred_keypoints_ndarray.reshape((-1, 2))
+        ax.scatter(pred_keypoints_matrix[:, 0], pred_keypoints_matrix[:, 1], marker='x', c='r')
+        
+    return fig, ax
